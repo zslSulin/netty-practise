5e7879d8bfeb7d905fb29559875e785138101d32
zslSulin <zsldeyou@126.com>
2018-10-23 23:36:40 +0800

    Initial commit

---
+++ src/main/java/com/mine/netty/NettyPractiseApplication.java
@@ -0,0 +1,12 @@
+package com.mine.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyPractiseApplication.class, args);
+    }
+}
