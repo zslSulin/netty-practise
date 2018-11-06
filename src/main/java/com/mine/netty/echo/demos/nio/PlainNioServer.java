63684fcc187f63cab35e026964482e76ee3032b5
张苏林 <zhangsl@51jnq.com>
2018-11-06 19:41:02 +0800

    oio nio server

---
+++ src/main/java/com/mine/netty/echo/demos/nio/PlainNioServer.java
@@ -0,0 +1,71 @@
+package com.mine.netty.echo.demos.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * PlainNioServer
+ *
+ * @author zhangsl
+ * @date 2018/11/6 15:15
+ */
+public class PlainNioServer {
+    public void serve(int port) throws IOException {
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+        ServerSocket socket = serverChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        Selector selector = Selector.open();
+
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+        for (;;) {
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
+                        System.out.println("Accept connection form " + client);
+                    }
+                    if (key.isWritable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
+                        while (buffer.hasRemaining()) {
+                            if (client.write(buffer) == 0) {
+                                break;
+                            }
+                        }
+                        client.close();
+                    }
+                } catch (IOException e) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException cex) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/mine/netty/echo/demos/oio/PlainOioServer.java
@@ -0,0 +1,46 @@
+package com.mine.netty.echo.demos.oio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * PlainOioServer
+ *
+ * @author zhangsl
+ * @date 2018/11/6 14:58
+ */
+public class PlainOioServer {
+    public void serve(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for (;;) {
+                final Socket clientSocket = socket.accept();
+                System.out.println(
+                        "Accepted connection from " + clientSocket
+                );
+                new Thread(() -> {
+                    OutputStream out;
+                    try {
+                        out = clientSocket.getOutputStream();
+                        out.write("Hi!\r\n".getBytes(Charset.forName("UTF-8")));
+                        out.flush();
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            clientSocket.close();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
